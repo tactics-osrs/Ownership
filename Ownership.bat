771a53bac391be307d112ef36d749bf4806f3912
Tactics <76490725+tactics-osrs@users.noreply.github.com>
2024-03-01 14:33:27 -0500

    Update Ownership.bat

--- Ownership.bat
+++ Ownership.bat
@@ -1,9 +1,13 @@
 @echo off
+:loop
 set /p "path=Path of file/folder you want to take full ownership of: "
 if exist "%path%" (
     takeown /F "%path%" /R /D Y
     icacls "%path%" /grant "%USERNAME%":(F) /T /C /Q
     echo Ownership taken and full control permissions granted for %path%
+    pause
 ) else (
     echo The path %path% does not exist. Please enter a valid path.
+    pause
+    goto loop
 )
